6a6d1438798dcb680dc378eab14cc3e8630838bb
falentio <riv.kevinfalent.io@gmail.com>
2023-06-02 10:43:49 +0700

    Revert "add experimental fast mode"
    This reverts commit 40dd3269693bc425b2b1c1011f46f24ee3adcc43.

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,8 +6,7 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/api/search.ts";
-import * as $3 from "./routes/api/ws.ts";
-import * as $4 from "./routes/index.tsx";
+import * as $3 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/Search.tsx";
 
@@ -16,8 +15,7 @@ const manifest = {
 		"./routes/[name].tsx": $0,
 		"./routes/api/joke.ts": $1,
 		"./routes/api/search.ts": $2,
-		"./routes/api/ws.ts": $3,
-		"./routes/index.tsx": $4,
+		"./routes/index.tsx": $3,
 	},
 	islands: {
 		"./islands/Counter.tsx": $$0,
